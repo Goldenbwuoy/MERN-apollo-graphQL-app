5cdace8c48dd07b9e8d30915e9f53ba6a4f0db98
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-06-13 20:57:19 +0800

    create a custom scalar type for timestamps

--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,7 +1,9 @@
 const Query = require("./query.resolver");
 const Mutation = require("./mutation.resolver");
+const { GraphQLDateTime } = require("graphql-iso-date");
 
 module.exports = {
 	Query,
 	Mutation,
+	DateTime: GraphQLDateTime,
 };
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,14 @@
 const { gql } = require("apollo-server-express");
 
 module.exports = gql`
+	scalar DateTime
+
 	type Note {
 		id: ID!
 		content: String!
 		author: String!
+		createdAt: DateTime!
+		updatedAt: DateTime!
 	}
 
 	type Query {
