ab0c793c418424f6ca9fa668186325e2a8863449
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-06-19 21:13:50 +0800

    setup signup and signin resolvers

--- server/resolvers/mutation.resolver.js
+++ server/resolvers/mutation.resolver.js
@@ -1,3 +1,11 @@
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const {
+	AuthenticationError,
+	ForbiddenError,
+} = require("apollo-server-express");
+
 module.exports = {
 	newNote: async (parent, args, { models }) => {
 		return await models.Note.create({
@@ -19,5 +27,45 @@ module.exports = {
 			{ $set: { content } },
 			{ new: true }
 		);
+	},
+	signUp: async (parent, { username, email, password }, { models }) => {
+		email = email.trim().toLowerCase();
+		const hashed = await bcrypt.hash(password, 10);
+		try {
+			const user = await models.User.create({
+				username,
+				email,
+				password: hashed,
+			});
+			return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+		} catch (err) {
+			console.log(err);
+
+			throw new Error("Error creating account");
+		}
+	},
+
+	signIn: async (parent, { username, email, password }, { models }) => {
+		if (email) {
+			// normalize email
+			email = email.trim().toLowerCase();
+		}
+		const user = await models.User.findOne({
+			$or: [{ email }, { username }],
+		});
+
+		//throw authentication error if no user is found
+		if (!user) {
+			throw new AuthenticationError("Error signing in (user not found)");
+		}
+
+		//throw an authentication error if passwords don't match
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid) {
+			throw new AuthenticationError(
+				"Error signing in (invalid password)"
+			);
+		}
+		return jwt.sign({ id: user._id }, process.env.JWT_SECRET);
 	},
 };
