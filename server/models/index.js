838a52bdc90a7780c4c5939f94b14966825c6c12
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-06-19 20:11:59 +0800

    add User type and create User model

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,9 @@
 const Note = require("./note.model");
+const User = require("./user.model");
 
 const models = {
 	Note,
+	User,
 };
 
 module.exports = models;
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,9 +6,17 @@ module.exports = gql`
 	type Note {
 		id: ID!
 		content: String!
-		author: String!
+		author: User!
 		createdAt: DateTime!
 		updatedAt: DateTime!
+	}
+
+	type User {
+		id: ID!
+		username: String!
+		email: String!
+		avatar: String!
+		notes: [Note!]!
 	}
 
 	type Query {
@@ -21,5 +29,7 @@ module.exports = gql`
 		newNote(content: String!): Note!
 		updateNote(id: ID!, content: String!): Note!
 		deleteNote(id: ID!): Boolean
+		signUp(username: String!, email: String!, password: String!): String!
+		signIn(username: String, email: String, password: String!): String!
 	}
 `;
