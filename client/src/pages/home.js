90ccf0be1fc24cd95058125cc5eba9205a81a197
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-06-25 12:18:29 +0800

    header and navigation components

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,20 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
 	return (
 		<div>
-			<h1>Notedly</h1>
-			<p>This is the Home page</p>
-
-			<ul>
-				<li>
-					<Link to="/mynotes">My Notes</Link>
-				</li>
-				<li>
-					<Link to="/favourites">Favourites</Link>
-				</li>
-			</ul>
+			<Header />
+			<Navigation />
 		</div>
 	);
 };
