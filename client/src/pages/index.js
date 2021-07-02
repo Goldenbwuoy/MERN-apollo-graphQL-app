de8fe7373c88b2c5b0f584abf7b68370242efe00
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-07-02 13:19:46 +0800

    setup PrivateRoute component to protect private routes from unauthorized access

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
+import { gql, useApolloClient } from "@apollo/client";
 
 import Home from "./home";
 import MyNotes from "./mynotes";
@@ -9,19 +10,50 @@ import Layout from "../components/Layout";
 import SignUp from "./signup";
 import SignIn from "./signin";
 
+const IS_LOGGED_IN = gql`
+	query ReadStatus {
+		status {
+			isLoggedIn
+		}
+	}
+`;
+
 const Pages = () => {
 	return (
 		<Router>
 			{/* Wrap the routes within the Layout component */}
 			<Layout>
 				<Route exact path="/" component={Home} />
-				<Route path="/mynotes" component={MyNotes} />
-				<Route path="/favourites" component={Favourites} />
+				<PrivateRoute path="/mynotes" component={MyNotes} />
+				<PrivateRoute path="/favourites" component={Favourites} />
 				<Route path="/note/:id" component={NotePage} />
 				<Route path="/signup" component={SignUp} />
 				<Route path="/signin" component={SignIn} />
 			</Layout>
 		</Router>
+	);
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	const client = useApolloClient();
+	const { status } = client.readQuery({ query: IS_LOGGED_IN });
+
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				status.isLoggedIn === true ? (
+					<Component {...props} />
+				) : (
+					<Redirect
+						to={{
+							pathname: "/signin",
+							state: { from: props.location },
+						}}
+					/>
+				)
+			}
+		/>
 	);
 };
 
