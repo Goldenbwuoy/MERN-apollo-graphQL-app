680489f225ebd69eeec4ef29d3d79a530b618935
Goldenbwuoy <mumanikidzwag@gmail.com>
2021-07-03 10:45:26 +0800

    fetch and display mynotes and favourites

--- client/src/gql/queries.js
+++ client/src/gql/queries.js
@@ -44,4 +44,44 @@ const IS_LOGGED_IN = gql`
 	}
 `;
 
-export { GET_NOTES, GET_NOTE, IS_LOGGED_IN };
+const GET_MY_NOTES = gql`
+	query me {
+		me {
+			id
+			username
+			notes {
+				id
+				createdAt
+				content
+				favouriteCount
+				author {
+					username
+					id
+					avatar
+				}
+			}
+		}
+	}
+`;
+
+const GET_MY_FAVOURITES = gql`
+	query me {
+		me {
+			id
+			username
+			favourites {
+				id
+				createdAt
+				content
+				favouriteCount
+				author {
+					id
+					username
+					avatar
+				}
+			}
+		}
+	}
+`;
+
+export { GET_NOTES, GET_NOTE, IS_LOGGED_IN, GET_MY_NOTES, GET_MY_FAVOURITES };
